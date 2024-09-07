3a6b45c2a683490ea892709de716f2b096fd819a
parza <parzaps4@gmail.com>
2024-09-07 21:42:34 +0330

    hi

--- app.py
+++ app.py
@@ -110,4 +110,5 @@ def submit_review(form_id):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
